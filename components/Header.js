a629818bfcd6a021c3c9eeb6a162cc3c0407bdb4
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-09-05 19:15:34 +1000

    Search Section Complete

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/solid";
 function Header() {
   return (
-    <header className='sticky top-0 z-50 grid grid-col-3 bg-white-500 shadow-md py-5 px-5'>
+    <header className='sticky top-0 z-50 grid grid-cols-3 bg-white-500 shadow-md py-5 px-5 md:px-10'>
       {/* left */}
       <div className='relative flex items-center h-10 cursor-pointer my-auto'>
         <Image
@@ -12,9 +13,16 @@ function Header() {
         />
       </div>
       {/* Middle */}
-      <div></div>
+      <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
+        <input
+          className='flex-grow pl-5 bg-transparent outline-none text-gray-600 placeholder-gray-400'
+          type='text'
+          placeHolder='Start Your Search'
+        />
+        <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2' />
+      </div>
       {/* Right */}
-      <div></div>
+      <div>asdasdas</div>
     </header>
   );
 }
