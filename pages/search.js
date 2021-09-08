ca682e9141e33055ae33296e74c846674d06a809
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-09-08 11:02:11 +1000

    Complete Map Section

--- pages/search.js
+++ pages/search.js
@@ -3,7 +3,7 @@ import Footer from "../components/Footer";
 import { useRouter } from "next/dist/client/router";
 import { format } from "date-fns";
 import InfoCard from "../components/infoCard";
-
+import Map from "../components/Map";
 function search({ searchResult }) {
   const router = useRouter();
   const { location, startDate, endDate, noOfGuests } = router.query;
@@ -43,6 +43,9 @@ function search({ searchResult }) {
               )
             )}
           </div>
+        </section>
+        <section className='hidden xl:inline-flex xl:min-w-[600px]'>
+          <Map searchResult={searchResult} />
         </section>
       </main>
       <Footer />
